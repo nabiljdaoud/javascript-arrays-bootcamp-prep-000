11111cbe84740d58870efe386643aed99ec354dc
Nabil Daoud <nabiljdaoud@gmail.com>
2018-03-08 00:58:30 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var ingredient1 = "bread";
+var ingredient2 = "mild cheese";
+var ingredient3 = 'sharp cheese';
+var ingredient4 = 'butter';
+var ingredient5 = 'tomato';
+var ingredient6 = 'garlic';
+
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'];
+  
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'];
+  
+var chocolateBars = [
+  'snickers', 
+  'hundred grand',
+  'kitkat',
+  'skittles'];
+  
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element); // = [element, ...array];
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element) // = [...array, element];
+  return array;
+}
