d943ca04786b36aeb5b1ebbfe41ff2b8479c8d35
Nabil Daoud <nabiljdaoud@gmail.com>
2018-03-08 01:13:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -63,5 +63,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, -1)
+  return array.slice(0, array.length -1) // array.slice(0, array.length - 1)
 }
